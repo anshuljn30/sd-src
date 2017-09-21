0e2d730d27653a5557c6b89fbe496fef9737d264
sumanthamarchinta <pallu123>
2017-09-20 21:17:55 -0400

    commiting get_data_test verified that it works now.

--- get_data_test.py
+++ get_data_test.py
@@ -1,4 +1,5 @@
 import db_tools
+import basic_tools
 
 issuer_id = "'1','2'"
 item_id=10
@@ -9,7 +10,7 @@ to_date=20100101
 df=db_tools.get_fundamental_data(issuer_id,item_id,periodicity,from_date,to_date,db_tools.connect_db())
 print(df)
 
-df['start_date'] = df['start_date'].apply(lambda x: db_tools.eom(str(x),"%Y%m%d"))
+df['start_date'] = basic_tools.eom(df['start_date'],"%Y%m%d")
 print (df)
 
 df = df.pivot(index='issuer_id',columns='start_date',values='numeric_value')
