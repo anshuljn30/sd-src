a75961becb9d6935ac19eb4d794043d0976b9146
sumanthamarchinta <pallu123>
2017-10-01 21:59:56 -0400

    tested the signal testing methods. works fine. you would need scores, returns, sector and signal test excel file to execute this test.

---
+++ test_signaltesting_methods.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import calendar
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import signal_test_tools
+import basic_tools
+
+open = True
+nmon = 1
+scores = pd.read_csv("C:/Investment_research/scores.csv", sep=',')
+returns = pd.read_csv("C:/Investment_research/returns.csv", sep=',')
+sector = pd.read_csv("C:/Investment_research/data/data_base_static/issuer_master_sector.csv",sep=',')
+file = "C:\Investment_research\signal_test.xlsx"
+
+signal_test_tools.signal_test_write_returns(scores,returns,nmon,file,False)
+
+signal_test_tools.signal_test_write_ic(scores,returns,sector,nmon,file,open)
+
+
+
