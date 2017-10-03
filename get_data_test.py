56b868349e3517c0eb113be2b84a8d20e9c8c8fa
sumanthamarchinta <pallu123>
2017-10-02 22:05:05 -0400

    added write_to_sheet method; input variables are dataframe, filename, sheet name and a variable: True for open excel after writing, False for run everything in background and dont open excel.

--- get_data_test.py
+++ get_data_test.py
@@ -1,17 +1,26 @@
 import db_tools
 import basic_tools
 
-issuer_id = "'1','2'"
+issuer_id = "select distinct issuer_id from issuer_master"
 item_id=10
 periodicity=1
 from_date=20050101
-to_date=20100101
+to_date=20160101
+
 
 df=db_tools.get_fundamental_data(issuer_id,item_id,periodicity,from_date,to_date,db_tools.connect_db())
-print(df)
-
 df['start_date'] = basic_tools.eom(df['start_date'],"%Y%m%d")
-print (df)
-
+df = df.drop_duplicates(['issuer_id','start_date'],keep='last')
 df = df.pivot(index='issuer_id',columns='start_date',values='numeric_value')
-print (df)
+
+df = basic_tools.fill_forward(df,0)
+
+
+
+
+
+
+
+
+
+
