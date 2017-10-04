9a5cd9f5ea3ec729635c9b56dab0dacf6613bea1
Anshul Jain <anshul.work17@gmail.com>
2017-10-03 18:43:06 -0700

    Adding function to convert frequency of data frame

--- basic_tools.py
+++ basic_tools.py
@@ -4,6 +4,24 @@ import datetime
 from pandas.tseries.offsets import *
 import xlwings as xw
 
+
+def random_frame():
+    dates = pd.date_range('20050101', '20060101', freq='d')
+    data_matrix = np.random.randint(0, 100, size=(366, 5))
+    data = pd.DataFrame(data_matrix, index=dates)
+    return data
+
+
+def convert_to(data, frequency):
+    dates = data.index
+    start_date = dates.min()
+    end_date = dates.max()
+
+    dates = pd.date_range(start_date, end_date, freq=frequency.lower())
+    data = data.reindex(dates)
+    data = data.ffill()
+    return data
+
 def fill_forward(df,column_from):
     for i in range (0,len(df.index)):
         for j in range(column_from+1,len(df.columns)):
