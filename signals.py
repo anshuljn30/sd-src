f21972359cb0138714ec0dc7ea9f933e15c8ac11
sumanthamarchinta <pallu123>
2017-11-13 20:32:32 -0500

    using sector from company class

--- signals.py
+++ signals.py
@@ -7,7 +7,7 @@ def e2p(dates,ids):
     return e2p
 
 def ebit2mktcap(dates,ids):
-    ebit = cdt.get_clean_data('EBIT', dates, ids)
+    ebit = cdt.get_clean_data('EBIT',  dates=dates, ids=ids)
     mkt_cap = cdt.get_clean_data('CompanyMcapLocal',dates, ids)
     ebit2mktcap = ebit/mkt_cap
     return ebit2mktcap
